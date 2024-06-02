19bc7cf59a6160e05d184ab39f6f68a9eed2d915
somerandomuser023 <ahmetmuec@gmail.com>
2024-06-02 12:08:31 +0200

    Hard coded data for assets finished

--- Backend/server.js
+++ Backend/server.js
@@ -6,7 +6,7 @@ const app = express(); // Create an instance of the express application
 app.use(cors()); // Enable CORS policy for all routes
 app.use(express.json()); // Middleware to parse JSON request bodies
 
-
+// Dummy data for contracts
 // Dummy data for contracts
 const contracts = {
   1: [
@@ -91,7 +91,7 @@ const contracts = {
     },
   ],
   3: [
-    /* {
+    {
       id: '8',
       assetId: '3',
       abschlussdatum: '31.12.2022',
@@ -112,7 +112,7 @@ const contracts = {
       laufzeit: '240 Monate',
       gesamtinvestition: '12000',
       abschlussgebuehr: '800',
-    }, */
+    },
   ],
   4: [
     {
@@ -398,97 +398,83 @@ app.get("/get_assets", (req, res) => {
   console.log("Made connection to /assets (Server)");
   const leasingAssets = [
     {
-      id: 1,
-      standort: 'Berlin',
-      leasingobjekt: 'Car',
-      leasinggeber: 'Company A',
-      leasingnehmer: 'John Doe',
+      id: 5472,
+      objektartBezeichnung: 'Parkhaus',
+      strasse: 'Froschgasse 93',
+      plz: '58912',
+      ort: 'Bierstadt',
+      zustand: 'Neubau',
+      objekterrichtung: 'Sale and lease back',
+      grundstuecksart: 'Kauf',
+      untermieter: 'Nein',
       imageUrl: 'https://media.gettyimages.com/id/128502214/de/foto/classic-turn-of-the-century-american-house.jpg?s=612x612&w=gi&k=20&c=d4GVWJ7ehp10xUREyj0787VvZ1Aedxts9ZNSMiuL0_Y=',
       vertraege: getContract(1),
-
     },
     {
-      id: 2,
-      standort: 'Berlin',
-      leasingobjekt: 'Car',
-      leasinggeber: 'Company A',
-      leasingnehmer: 'John Doe',
+      id: 6160,
+      objektartBezeichnung: 'Lagerhalle',
+      strasse: 'Weingasse 11',
+      plz: '58911',
+      ort: 'Hasstadt',
+      zustand: 'Neubau',
+      objekterrichtung: 'Projektmanagement',
+      grundstuecksart: 'Erbbaurecht',
+      untermieter: 'Nein',
       imageUrl: 'https://www.wilsonhomes.com.au/sites/default/files/styles/blog_hero_banner/public/My%20project%20-%202023-06-20T095818.329%20%281%29_0.jpg?itok=UbtVbhT0',
       vertraege: getContract(2),
-
     },
     {
-      id: 3,
-      standort: 'Berlin',
-      leasingobjekt: 'Car',
-      leasinggeber: 'Company A',
-      leasingnehmer: 'John Doe',
+      id: 7845,
+      objektartBezeichnung: 'Lagerhalle',
+      strasse: 'Froschgasse 78',
+      plz: '58912',
+      ort: 'Bierstadt',
+      zustand: 'Neubau',
+      objekterrichtung: 'Sale and lease back',
+      grundstuecksart: 'Kauf',
+      untermieter: 'Ja',
       imageUrl: 'https://cdna.artstation.com/p/assets/images/images/045/535/208/large/eugene-caubel-haussmann-concept.jpg?1642963048',
       vertraege: getContract(3),
-
     },
-
     {
-      id: 4,
-      standort: 'Berlin',
-      leasingobjekt: 'Car',
-      leasinggeber: 'Company A',
-      leasingnehmer: 'John Doe',
+      id: 9017,
+      objektartBezeichnung: 'Lagerhalle',
+      strasse: 'Weingasse 11',
+      plz: '58911',
+      ort: 'Hasstadt',
+      zustand: 'Neubau',
+      objekterrichtung: 'Projektmanagement',
+      grundstuecksart: 'Erbbaurecht',
+      untermieter: 'Nein',
       imageUrl: 'https://butterflymx.com/wp-content/uploads/2022/07/asset-management-vs-property-management.jpg',
       vertraege: getContract(4),
-
     },
     {
-      id: 5,
-      standort: 'Berlin',
-      leasingobjekt: 'Car',
-      leasinggeber: 'Company A',
-      leasingnehmer: 'John Doe',
+      id: 8542,
+      objektartBezeichnung: 'Produktionsgebäude',
+      strasse: 'Froschgasse 85',
+      plz: '58912',
+      ort: 'Bierstadt',
+      zustand: 'Neubau',
+      objekterrichtung: 'Sale and lease back',
+      grundstuecksart: 'Kauf',
+      untermieter: 'Ja',
       imageUrl: 'https://d1y8sb8igg2f8e.cloudfront.net/images/Hey_Congress_No_Time_to_Leave_the_Dance_Floor_.width-600.jpg',
       vertraege: getContract(5),
-
     },
     {
-      id: 6,
-      standort: 'Berlin',
-      leasingobjekt: 'Car',
-      leasinggeber: 'Company A',
-      leasingnehmer: 'John Doe',
-      imageUrl: 'https://cdn1.vogel.de/blLu-w9zy244Zp6RW5E6AjazJeU=/fit-in/800x0/p7i.vogel.de/wcms/26/9a/269a43221f58018f0bcabfb125f74cb4/0109208144.jpeg',
+      id: 5509,
+      objektartBezeichnung: 'Sondergebäude',
+      strasse: 'Weingasse 13',
+      plz: '58911',
+      ort: 'Hasstadt',
+      zustand: 'Neubau',
+      objekterrichtung: 'Projektmanagement',
+      grundstuecksart: 'Erbbaurecht',
+      untermieter: 'Nein',
+      imageUrl: 'https://media.gettyimages.com/id/128502214/de/foto/classic-turn-of-the-century-american-house.jpg?s=612x612&w=gi&k=20&c=d4GVWJ7ehp10xUREyj0787VvZ1Aedxts9ZNSMiuL0_Y=',
       vertraege: getContract(6),
-      
     },
-    {
-      id: 7,
-      standort: 'Berlin',
-      leasingobjekt: 'Car',
-      leasinggeber: 'Company A',
-      leasingnehmer: 'John Doe',
-      imageUrl: 'https://media.gettyimages.com/id/128502214/de/foto/classic-turn-of-the-century-american-house.jpg?s=612x612&w=gi&k=20&c=d4GVWJ7ehp10xUREyj0787VvZ1Aedxts9ZNSMiuL0_Y=',
-      vertraege: getContract(7),
-
-    },
-    {
-      id: 8,
-      standort: 'Berlin',
-      leasingobjekt: 'Car',
-      leasinggeber: 'Company A',
-      leasingnehmer: 'John Doe',
-      imageUrl: 'https://www.wilsonhomes.com.au/sites/default/files/styles/blog_hero_banner/public/My%20project%20-%202023-06-20T095818.329%20%281%29_0.jpg?itok=UbtVbhT0',
-      vertraege: getContract(8),
-
-    },
-    {
-      id: 9,
-      standort: 'Berlin',
-      leasingobjekt: 'Car',
-      leasinggeber: 'Company A',
-      leasingnehmer: 'John Doe',
-      imageUrl: 'https://media.gettyimages.com/id/128502214/de/foto/classic-turn-of-the-century-american-house.jpg?s=612x612&w=gi&k=20&c=d4GVWJ7ehp10xUREyj0787VvZ1Aedxts9ZNSMiuL0_Y=',
-      vertraege: getContract(9),
-      
-    },
-    // More assets
   ];
 
   res.json(leasingAssets);
@@ -499,35 +485,30 @@ app.get("/get_msges", (req, res) => {
   console.log("Made connection to /get_msges (Server)");
   const msges = [
     {
-      Gelesen: "0",
       Betreff: "Neues Datenschutz Gesetz",
       Sender: "DAL@gmail.com",
       Vorschau: "Wir haben ein neues...",
       Message: "Sehr geehrte Damen und Herren, \n\nwir möchten Sie darüber informieren, dass ein neues Datenschutzgesetz in Kraft getreten ist. Bitte lesen Sie die angehängten Dokumente für weitere Informationen.\n\nMit freundlichen Grüßen,\nIhr Datenschutz-Team"
     },
     {
-      Gelesen: "1",
       Betreff: "Wichtige Informationen zur Wartung",
       Sender: "service@webmail.com",
       Vorschau: "Am kommenden Wochenende werden...",
       Message: "Liebe Kunden, \n\nbitte beachten Sie, dass am kommenden Wochenende Wartungsarbeiten an unseren Servern durchgeführt werden. In dieser Zeit wird es zu Unterbrechungen im Service kommen. Wir entschuldigen uns für die Unannehmlichkeiten.\n\nBeste Grüße,\nIhr Webmail Service-Team"
     },
     {
-      Gelesen: "1",
       Betreff: "Einladung zum Meeting",
       Sender: "hr@unternehmen.de",
       Vorschau: "Bitte bestätigen Sie Ihre Teilnahme an...",
       Message: "Hallo, \n\nwir laden Sie herzlich zu unserem jährlichen Mitarbeiter-Meeting ein. Das Meeting findet am 15. Juni um 10:00 Uhr im Konferenzraum A statt. Bitte bestätigen Sie Ihre Teilnahme bis zum 10. Juni.\n\nVielen Dank,\nIhr HR-Team"
     },
     {
-      Gelesen: "1",
       Betreff: "Wichtige Informationen zur Wartung",
       Sender: "service@webmail.com",
       Vorschau: "Am kommenden Wochenende werden...",
       Message: "Liebe Kunden, \n\nbitte beachten Sie, dass am kommenden Wochenende Wartungsarbeiten an unseren Servern durchgeführt werden. In dieser Zeit wird es zu Unterbrechungen im Service kommen. Wir entschuldigen uns für die Unannehmlichkeiten.\n\nBeste Grüße,\nIhr Webmail Service-Team"
     },
     {
-      Gelesen: "1",
       Betreff: "Einladung zum Meeting",
       Sender: "hr@unternehmen.de",
       Vorschau: "Bitte bestätigen Sie Ihre Teilnahme an...",
@@ -567,7 +548,6 @@ app.get("/get_verträge", (req, res) => {
   res.json(verträge);
 });
 
-// Start the server on port 3002
 const port = 3002;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
